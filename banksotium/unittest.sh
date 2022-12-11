e33618f44a0911c487bdf8d6a9e3f08c7c0a2db0
Aadharsh Kannan <akannan@microsoft.com>
2022-12-11 15:52:15 -0800

    Unit test running without cert issues

--- banksotium/unittest.sh
+++ banksotium/unittest.sh
@@ -18,25 +18,23 @@ echo "Waiting for Banksotium to start..."
 #Copying this logic from Takuro's App in https://github.com/takuro-sato/ccf-app-template/blob/9c4e5e9b45330aaab35e20b3548731ee1b8a450d/banking-app/test.sh
 a=1
 retcode="000"
-while [ "000" == $retcode ]
+while [ "200" != $retcode ]
 do
-    retcode="$(curl $server/commit -X GET --cacert service_cert.pem --cert user0_cert.pem --key user0_privk.pem $only_status_code)"
+    retcode="$(curl $server/app/commit -X GET --cacert ./workspace/sandbox_common/service_cert.pem $only_status_code)"
     sleep 1
     echo "elapsed $a seconds. Server returned $retcode"    
     ((a+=1))
 done
 
-if [ $retcode != "200" ]
-then
-    echo "Issue with the deployment. Let's exit!"
-    exit $retcode  
-fi
+centralBank_id=$(openssl x509 -in "./workspace/sandbox_common/user0_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
+bank1_id=$(openssl x509 -in "./workspace/sandbox_common/user1_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
+bank2_id=$(openssl x509 -in "./workspace/sandbox_common/user2_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
+bank3_id=$(openssl x509 -in "./workspace/sandbox_common/user3_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
+bank4_id=$(openssl x509 -in "./workspace/sandbox_common/user4_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
+bank5_id=$(openssl x509 -in "./workspace/sandbox_common/user5_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
 
-centralBank_id=$(openssl x509 -in "user0_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
-bank1_id=$(openssl x509 -in "user1_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
-bank2_id=$(openssl x509 -in "user2_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
-bank3_id=$(openssl x509 -in "user3_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
-bank4_id=$(openssl x509 -in "user4_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
-bank5_id=$(openssl x509 -in "user5_cert.pem" -noout -fingerprint -sha256 | cut -d "=" -f 2 | sed 's/://g' | awk '{print tolower($0)}')
+echo "Register the Central Bank as $centralBank_id"
 
-echo "Register the Central Bank"
+echo "OK"
+kill -9 $sandbox_pid
+exit 0
